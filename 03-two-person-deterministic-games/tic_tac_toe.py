7a578a91884a3775a73bdcad6c6a633228f4affc
Patrycja Wysocka <p.wysocka@stud.elka.pw.edu.pl>
2021-11-22 08:43:05 +0100

    clean, working with ab pruning

--- 03-two-person-deterministic-games/tic_tac_toe.py
+++ 03-two-person-deterministic-games/tic_tac_toe.py
@@ -37,7 +37,7 @@ class TicTacToe():
             x, y = coord
             self.board[x][y] = player
             self.free_spots.remove((x, y))
-    
+
     def undo_move(self, coord):
         x, y = coord
         self.board[x][y] = ' '
@@ -59,7 +59,7 @@ class TicTacToe():
         # diagonal
         if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
             winner = self.board[0][0]
-        
+
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
             winner = self.board[0][2]
 
@@ -73,7 +73,6 @@ class TicTacToe():
         coord = choice(self.free_spots)
         self.make_move(coord, player)
 
-
     def minimax(self, depth, is_max):
 
         # check if someone wins (or there is a tie)
@@ -85,7 +84,7 @@ class TicTacToe():
             return self.score[self.check_winner()]*depth
 
         player = (self.max_player if is_max else self.min_player)
-        
+
         score = []
         for x in range(3):
             for y in range(3):
@@ -94,10 +93,10 @@ class TicTacToe():
                     self.make_move(spot, player)
                     score.append(self.minimax(depth-1, not is_max))
                     self.undo_move(spot)
-        
+
         return max(score) if is_max else min(score)
 
-    def make_best_move_max(self, player):
+    def make_best_move_max(self, player, depth, ab,):
         best_score = -inf
         best_move = None
 
@@ -106,7 +105,7 @@ class TicTacToe():
                 spot = x, y
                 if self.is_free(spot):
                     self.make_move(spot, player)
-                    score = self.minimax(len(self.free_spots), False)
+                    score = self.alfa_beta(depth, False, -inf, inf) if ab else self.minimax(depth, False)
                     self.undo_move(spot)
 
                     if score > best_score:
@@ -116,7 +115,7 @@ class TicTacToe():
         print(f'max looked states: {self.max_states}')
         self.make_move(best_move, player)
 
-    def make_best_move_min(self, player):
+    def make_best_move_min(self, player, depth, ab):
         best_score = inf
         best_move = None
 
@@ -125,7 +124,7 @@ class TicTacToe():
                 spot = x, y
                 if self.is_free(spot):
                     self.make_move(spot, player)
-                    score = self.minimax(len(self.free_spots), True)
+                    score = self.alfa_beta(depth, True, -inf, inf) if ab else self.minimax(depth, True)
                     self.undo_move(spot)
 
                     if score < best_score:
@@ -135,70 +134,44 @@ class TicTacToe():
         print(f'min looked states: {self.min_states}')
         self.make_move(best_move, player)
 
-    # def alfa_beta(self, is_max, depth, alfa, beta):
-    #     # check if someone wins (or there is a tie)
-    #     if depth == 0 or self.check_winner() is not None:
-    #         if is_max:
-    #             self.max_states += 1
-    #         else:
-    #             self.min_states += 1
-    #         return self.score[self.check_winner()]*depth
+    def alfa_beta(self, depth, is_max, alfa, beta):
+        # check if someone wins (or there is a tie)
+        if depth == 0 or self.check_winner() is not None:
+            if is_max:
+                self.max_states += 1
+            else:
+                self.min_states += 1
+            return self.score[self.check_winner()]*depth
 
-    #     player = (self.max_player if is_max else self.min_player)
-        
-    #     if is_max:
-    #         score = []
-    #         for x in range(3):
-    #             for y in range(3):
-    #                 spot = x, y
-    #                 if self.is_free(spot):
-    #                     self.make_move(spot, player)
-    #                     score.append(self.alfa_beta(depth-1, not is_max))
-    #                     self.undo_move(spot)
-        
-    #     return max(score) if is_max else min(score)
+        player = (self.max_player if is_max else self.min_player)
 
+        if is_max:
+            value = -inf
+            score = []
+            for x in range(3):
+                for y in range(3):
+                    spot = x, y
+                    if self.is_free(spot):
+                        self.make_move(spot, player)
+                        score.append(self.alfa_beta(depth-1, not is_max, alfa, beta))
+                        self.undo_move(spot)
+                        value = max(score)
+                        if value >= beta:
+                            break
+                    alfa = max(alfa, value)
+        else:
+            value = inf
+            score = []
+            for x in range(3):
+                for y in range(3):
+                    spot = x, y
+                    if self.is_free(spot):
+                        self.make_move(spot, player)
+                        score.append(self.alfa_beta(depth-1, not is_max, alfa, beta))
+                        self.undo_move(spot)
+                        value = min(score)
+                        if value <= alfa:
+                            break
+                    beta = min(beta, value)
 
-if __name__ == "__main__":
-    tic_tac_toe = TicTacToe('X')
-    tic_tac_toe.show_board()
-
-    moves = {
-        1: (0, 0),
-        2: (0, 1),
-        3: (0, 2),
-        4: (1, 0),
-        5: (1, 1),
-        6: (1, 2),
-        7: (2, 0),
-        8: (2, 1),
-        9: (2, 2)
-    }
-
-    
-    while tic_tac_toe.check_winner() is None:
-        tic_tac_toe.make_best_move_min('O')
-        tic_tac_toe.show_board()
-
-        if tic_tac_toe.check_winner() is not None:
-            print(tic_tac_toe.check_winner())
-            break
-
-
-        coord = input("make next move: ")
-        tic_tac_toe.make_move(moves[int(coord)], 'X')
-        tic_tac_toe.show_board()
-        tic_tac_toe.check_winner()
-
-        if tic_tac_toe.check_winner() is not None:
-            print(tic_tac_toe.check_winner())
-            break
-
-
-
-
-
-
-        
-
-    
+        return value
